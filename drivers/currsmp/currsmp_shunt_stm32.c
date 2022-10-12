0adc3c45ee8df221bbce4d1bd54b7550794185e0
Gerard Marull-Paretas <gerard@teslabs.com>
2022-10-12 10:42:57 +0200

    drivers: add missing headers
    IRQ/Kernel APIs were being used without including necessary headers. It
    worked before because of indirect includes.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- drivers/currsmp/currsmp_shunt_stm32.c
+++ drivers/currsmp/currsmp_shunt_stm32.c
@@ -7,6 +7,8 @@
 
 #include <zephyr/drivers/clock_control/stm32_clock_control.h>
 #include <zephyr/drivers/pinctrl.h>
+#include <zephyr/irq.h>
+#include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
 #include <stm32_ll_adc.h>
--- drivers/feedback/halls_stm32.c
+++ drivers/feedback/halls_stm32.c
@@ -8,6 +8,7 @@
 #include <zephyr/drivers/clock_control/stm32_clock_control.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/pinctrl.h>
+#include <zephyr/irq.h>
 #include <zephyr/logging/log.h>
 
 #include <stm32_ll_tim.h>
